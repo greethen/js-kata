6ae06727c2e2e86e8b86ebbcab387f18ebfb6cd5
Greta Gerulyte <greta@Gretas-MacBook-Air.local>
2018-05-14 14:26:49 +0200

    main structure

---
+++ w20monday/mysolution/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//const divs = document.querySelectorAll("#app div");
+const message = document.querySelector("#cookie_message");
+
+// let stuff = Array.from(divs);
+// let stuff = [...divs];
+message.addEventListener("click", function(){
+    message.transform = `translate(0vw)`;
+})
+//divs.forEach(function(div){
+    // div.style.borderRadius = "10%";
+    // div.style.left="50vw";
+    //div.style.left = `${Math.random()*100} vw`;
+    //div.style.transform = `translate(50vw, 50vh)`;
+//     div.addEventListener("mouseenter", function(){
+//         div.style.transform= `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
+//         div.style.backgroundColor = `hsl(${Math.random()*360}, ${Math.random()*100}%, 50%)`
+//         //#FF1234, rgb(0, 123, 345), hsl(360, 12%, 12%)
+//     })
+// })
+// let 
+// add.event.Listener ("cklick")
