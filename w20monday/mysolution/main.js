407bbd0184e055f6df93ec1aed2fd851b5eac397
greethen <gret2539@stud.kea.dk>
2018-05-15 10:23:11 +0200

    all files working

--- w20monday/mysolution/main.js
+++ w20monday/mysolution/main.js
@@ -1,23 +1,17 @@
 "use strict";
 
-//const divs = document.querySelectorAll("#app div");
-const message = document.querySelector("#cookie_message");
 
-// let stuff = Array.from(divs);
-// let stuff = [...divs];
-message.addEventListener("click", function(){
-    message.transform = `translate(0vw)`;
+//page loads
+const message = document.querySelector(".message");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("page loaded")
+    message.style.transform = `translateY(-10vh)`;
+
 })
-//divs.forEach(function(div){
-    // div.style.borderRadius = "10%";
-    // div.style.left="50vw";
-    //div.style.left = `${Math.random()*100} vw`;
-    //div.style.transform = `translate(50vw, 50vh)`;
-//     div.addEventListener("mouseenter", function(){
-//         div.style.transform= `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
-//         div.style.backgroundColor = `hsl(${Math.random()*360}, ${Math.random()*100}%, 50%)`
-//         //#FF1234, rgb(0, 123, 345), hsl(360, 12%, 12%)
-//     })
-// })
-// let 
-// add.event.Listener ("cklick")
+//the div slides after click the button
+const button = document.querySelector("button");
+button.addEventListener("click", function(){
+    console.log("clicked")
+    message.style.transform = `translateY(100vh)`;
+})
+
